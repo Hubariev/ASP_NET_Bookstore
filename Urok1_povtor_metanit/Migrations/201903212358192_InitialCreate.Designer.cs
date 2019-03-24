5c040cbb9093ac8b296a8b6a38ebfc14b50210ad
Hubariev <yevhenii.hubariev@gmail.com>
2019-03-24 16:12:43 +0100

    Migracja modeli dla Identity

--- Urok1_povtor_metanit/Migrations/201903212358192_InitialCreate.Designer.cs
+++ Urok1_povtor_metanit/Migrations/201903212358192_InitialCreate.Designer.cs
@@ -7,13 +7,13 @@ namespace Urok1_povtor_metanit.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class MigrateDB2 : IMigrationMetadata
+    public sealed partial class InitialCreate : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(MigrateDB2));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
         
         string IMigrationMetadata.Id
         {
-            get { return "201903171442358_MigrateDB2"; }
+            get { return "201903212358192_InitialCreate"; }
         }
         
         string IMigrationMetadata.Source
--- Urok1_povtor_metanit/Migrations/Configuration.cs
+++ Urok1_povtor_metanit/Migrations/Configuration.cs
@@ -5,14 +5,15 @@ namespace Urok1_povtor_metanit.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Urok1_povtor_metanit.Models.BookContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Urok1_povtor_metanit.Models.ApplicationContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
+            ContextKey = "Urok1_povtor_metanit.Models.ApplicationContext";
         }
 
-        protected override void Seed(Urok1_povtor_metanit.Models.BookContext context)
+        protected override void Seed(Urok1_povtor_metanit.Models.ApplicationContext context)
         {
             //  This method will be called after migrating to the latest version.
 
